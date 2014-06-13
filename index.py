a4d610aad376f26d7dcfc62cf8fdbeb0440efdb7
wasw100 <wasw100@gmail.com>
2014-06-13 23:02:09 +0800

    can index post

--- index.py
+++ index.py
@@ -5,7 +5,88 @@ create pylucene index
 
 from flask import current_app
 
+import sys, os, lucene, threading, time
+from datetime import datetime
+
+from java.io import File
+from org.apache.lucene.analysis.miscellaneous import LimitTokenCountAnalyzer
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType
+from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig
+from org.apache.lucene.store import SimpleFSDirectory
+from org.apache.lucene.util import Version
+
+
+class Ticker(object):
+
+    def __init__(self):
+        self.tick = True
+
+    def run(self):
+        while self.tick:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            time.sleep(1.0)
+
 
 def build_index():
-    store_dir = current_app.config['INDEX_STORE_DIR']
-    print store_dir
+
+    lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+    post_dir = current_app.config['LOCAL_REPO_PATH'] + '/_posts/'
+    index_store_dir = current_app.config['INDEX_STORE_DIR']
+    print post_dir
+    print index_store_dir
+
+    analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
+
+    store = SimpleFSDirectory(File(index_store_dir))
+    analyzer = LimitTokenCountAnalyzer(analyzer, 1048576)
+    config = IndexWriterConfig(Version.LUCENE_CURRENT, analyzer)
+    config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+    writer = IndexWriter(store, config)
+
+    indexDocs(post_dir, writer)
+    ticker = Ticker()
+    print 'commit index',
+    threading.Thread(target=ticker.run).start()
+    writer.commit()
+    writer.close()
+    ticker.tick = False
+    print 'done'
+
+
+def indexDocs(root, writer):
+        t1 = FieldType()
+        t1.setIndexed(True)
+        t1.setStored(True)
+        t1.setTokenized(False)
+        t1.setIndexOptions(FieldInfo.IndexOptions.DOCS_AND_FREQS)
+
+        t2 = FieldType()
+        t2.setIndexed(True)
+        t2.setStored(False)
+        t2.setTokenized(True)
+        t2.setIndexOptions(FieldInfo.IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+
+        for root, dirnames, filenames in os.walk(root):
+            print filenames
+            for filename in filenames:
+                if not filename.endswith('.md'):
+                    continue
+                print "adding", filename
+                try:
+                    path = os.path.join(root, filename)
+                    file = open(path)
+                    contents = unicode(file.read(), 'utf-8')
+                    file.close()
+                    doc = Document()
+                    doc.add(Field("name", filename, t1))
+                    doc.add(Field("path", root, t1))
+                    if len(contents) > 0:
+                        doc.add(Field("contents", contents, t2))
+                    else:
+                        print "warning: no content in %s" % filename
+                    writer.addDocument(doc)
+                except Exception, e:
+                    print "Failed in indexDocs:", e
